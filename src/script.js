e49364306bf3a5c7c320acf53e5685b3b18c3879
Aline Costa <incristn@gmail.com>
2024-05-24 19:56:02 -0300

    atualização de palpites falhos no script

--- src/script.js
+++ src/script.js
@@ -6,18 +6,21 @@ function jogoDeAdivinhacao() {
 
     if (!palpiteDigitado) {
         alert("Digite um valor válido!")
-    }
+        return;
+    }  
 
     if (palpiteDigitado === numeroAleatorio) {
         alert("É isso mesmo, você adivinhou!")
     } else if (palpiteDigitado > numeroAleatorio) {
-        atualizarFeedback("O número é muito alto! Tente novamente.")
-    } else {
-        atualizarFeedback("O número é muito baixo! Tente novamente.")
+        tentativas++;
+        atualizarFeedback("O número é muito alto! Tente novamente.");
+    } else if (palpiteDigitado < numeroAleatorio) {
+        tentativas++;
+        atualizarFeedback("O número é muito baixo! Tente novamente.");
     }
-
-
-
+    
+    const novaPontuacao = 100 - (tentativas * 10);
+    atualizarPontuacao(novaPontuacao);
 
 
 }
